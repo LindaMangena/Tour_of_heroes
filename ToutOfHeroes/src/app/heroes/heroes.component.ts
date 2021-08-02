aa93df999e877a3766a604e3da2766190b11e105
LindaMangena <lindajazz42@gmail.com>
2021-08-02 18:25:08 -0400

    getting heroes from the service

--- ToutOfHeroes/src/app/heroes/heroes.component.ts
+++ ToutOfHeroes/src/app/heroes/heroes.component.ts
@@ -14,15 +14,20 @@ export class HeroesComponent implements OnInit {
   heroes :Hero[]=[];
   selectedHero?: Hero;
 
-  constructor() { }
+  constructor(private heroService :HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes() : void{
+    this.heroes = this.heroService.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-}
+ }
   
 
 
