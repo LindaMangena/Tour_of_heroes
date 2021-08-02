93e7c224b23aeec8ed8fe44be21c723a5f53129a
academy_intern_mac05 <academy_intern_mac05@academy-intern-mac05s-MacBook-Air.local>
2021-08-02 12:17:14 +0200

    Added click event for getting hero's data

--- ToutOfHeroes/src/app/heroes/heroes.component.ts
+++ ToutOfHeroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +8,7 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
 
   hero: Hero = {
     id: 1,
@@ -14,10 +16,19 @@ export class HeroesComponent implements OnInit {
 
   }
 
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
-  }
+    selectedHero?: Hero;
+    onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+    }
+ 
 
-}
+    }
+
+  
+
+
